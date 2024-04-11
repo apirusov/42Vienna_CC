23f0746dfc2066e1bb7a387fa6b67cc0be390816
apirusov <157116164+apirusov@users.noreply.github.com>
2024-04-11 19:23:26 +0200

    Update ft_printf.c

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -185,17 +185,102 @@ EXAMPLE
 }
 }
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int print_char(int c)
+{
+       return (write(1, &c, 1));
+}
+
+int print_str(char *str)
+{
+      size_t count;
+      
+      while (*str)
+      {
+              print_char((int)*str);
+              count++;
+              str++;
+       }
+       return (count);
+}
+
+int print_digit(long n, int base)
+{
+       size_t count;
+       char    *symbols;
+
+      symbols = "0123456789abcdef";
+      count = 0;
+       if (n < 0)
+       {
+              write(1, "-", 1);
+              return (print_digit(-n, base) + 1);
+       }
+       else if (n < base)
+              return (print_char(symbols[n]));
+       else 
+       {
+              count = print_digit(n / base);
+              return (count + print_digit(n % base, base));
+       }
+}
+
+int print_format(char specifier, va_list ap)
+{
+       size_t count;
+
+       count = 0;
+       if (specifier == 'c')
+              count += print_char(va_arg(ap, int)); // char actually casts an int
+       else if (specifier == 's')
+              count += print_str(va_arg(ap, char *))
+       else if (specifier == 'd')
+              count += print_digit((long)(va_arg(ap, int)), 10);
+       else if (specifier == 'o')
+              count += print_digit((long)(va_arg(ap, int)), 8);
+       else if (specifier == 'x')
+              count += print_digit((long)(va_arg(ap, unsigned int)), 16);
+       else 
+              count += write(1, &specifier, 1);
+       return count;
+}
+
+int ft_printf(const char *s, ...)
+{
+	char	*format_spec;
+	size_t	i;
+       size_t count;
+	va_list	args;
+
+	va_start(args, s);
+       count = 0;
+       while (!*s)
+       {
+              if (*format == '%' && *(++format) != '%')
+                     count += print_format (*(++format), ap);
+              else
+                     count += write(1, format, 1);
+              ++format;
+       }
+       va_end(ap);
+       return (count);
+}
+
 #include <stdlib.h>
 #include <stdio.h>
 int main()
 {
-       char	*name = "Andrey";
-       unsigned int  date =  28;
-       int month = 3;
-       int year = 1989;
-       float age = 35.03;
-       int age_int = (int)age;
-       int i = 0;
+    char	*name = "Andrey";
+    unsigned int  date =  -28;
+    int month = 3;
+    int year = 1989;
+    float age = 35.03;
+    int age_int = (int)age;
+    int i = 0;
 	printf("printf :\nHello, \t%s, you are born at %u.%o.%d, which means you are %f years old \
        or %e%% of 100. It also is equal to %X or %x in \"hexadecimal\"\n", name, date, month, year, age, age, age_int, age_int);
        printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d \
@@ -208,6 +293,6 @@ int main()
        i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, \
        i, i, i, i, i, i, i, i);
        printf("Sum of 1, 2, and 3: %d\n", sum(3, 1, 2, 3));
-
+       ft_printf("ft_printf:\nHello, \t%s, you are born at %u.%o.%d, which means you are %d years old \
+       or %d%% of 100. It also is equal to %x or %x in \"hexadecimal\"\n", name, date, month, year, age, age, age_int, age_int);)
 }
-
