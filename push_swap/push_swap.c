0c4cf20ae441c8e63dbcc003a3dd4ca8e9ba1751
Andrei Pirusov <U0271HU@post.at>
2024-07-28 10:15:55 +0200

    push_swap added

---
+++ push_swap/push_swap.c
@@ -0,0 +1,26 @@
+#include "push_swap.h"
+
+int main (int ac, char **av)
+{
+    t_stack_node *a;
+    t_stack_node *b;
+
+    a = NULL;
+    b = NULL;
+    if (1 == ac || (2 == ac && !av[1][0]))
+        return (1);
+    else if (2 == ac)
+        av = ft_split(av[1], ' ');
+    stack_init(&a, av + 1, 2 == ac);
+    if (!stack_sorted(a))
+    {
+        if (stack_len(a) == 2)
+            sa(&a, false);
+        else if (stack_len(a) == 3)
+            tiny_sort(&a);
+        else
+            push_swap(&a, &b);
+    }
+    free_stack(&a);
+    
+}
